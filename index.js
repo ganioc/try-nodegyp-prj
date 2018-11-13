86b69ffc1129974de359d85ac5697e7f45211736
yango3 <o3.yang@hotmail.com>
2018-11-13 14:56:16 +0800

    1st commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var math = require('./lib/mymath');
+var result = null;
+
+result = math.add( 5,2);
+console.log("result is:", result);
+
---
+++ lib/math.cc
@@ -0,0 +1,13 @@
+#include "math.h"
+
+int add(int x , int y){
+  return x+y;
+}
+
+int minus( int x, int y){
+  return x-y;
+}
+
+int multiply( int x, int y){
+  return x*y;
+}
